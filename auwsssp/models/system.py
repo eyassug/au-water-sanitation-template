c610cf5cdbd2469b805e8f8ffef1716b02c1aa30
Samuel habtewold <andeabesha@gmail.com>
2014-07-24 23:57:24 +0300

    Added System Model definition

---
+++ auwsssp/models/system.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils.encoding import smart_unicode
+
+class System(models.Model):
+    name = models.CharField(max_length=120, null=False, blank=True)
+    isActive = models.BooleanField()
+ 
