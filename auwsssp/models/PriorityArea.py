b10914075499968214fffcbb3ec81591a6aa6286
Samuel habtewold <andeabesha@gmail.com>
2014-07-24 23:36:48 +0300

    modify model

--- auwsssp/models/PriorityArea.py
+++ auwsssp/models/PriorityArea.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils.encoding import smart_unicode
 
-countrycode = models.ForeignKey('CountryCode')
-name = models.CharField(max_length=100, null=False, blanck=True)
-Latitude = models.DecimalField(max_digits=19, decimal_places=10, null=False)
-longitude = models.DecimalField(max_digits=19, decimal_places=10, null=False)
-isActive = models.BooleanField()
+class PriorityArea(models.Model):
+    countrycode = models.ForeignKey('CountryCode')
+    name = models.CharField(max_length=100, null=False, blanck=True)
+    Latitude = models.DecimalField(max_digits=19, decimal_places=10, null=False)
+    longitude = models.DecimalField(max_digits=19, decimal_places=10, null=False)
+    isActive = models.BooleanField()
 
