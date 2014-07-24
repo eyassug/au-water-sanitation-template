690dd8d51710f658418f353c465ef159b64840bc
Samuel habtewold <andeabesha@gmail.com>
2014-07-25 01:47:34 +0300

    Added Tender Procedure property app files

---
+++ TenderProcedureProperty/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils.encoding import smart_unicode
+
+class TenderProcedureProperty(models.Model):
+    name = models.CharField(max_length=120, null=False, blank=True)
+    isActive = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
