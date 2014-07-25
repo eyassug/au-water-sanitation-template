7b68fdf4b325cff39f3f4be4b6d3d1ba8d563a26
Samuel habtewold <andeabesha@gmail.com>
2014-07-25 16:28:31 +0300

    Modified model plural names.

--- dashboard/models.py
+++ dashboard/models.py
@@ -31,18 +31,28 @@ class AccessProperty(models.Model):
     name = models.CharField(max_length=120, null=False, blank=False)
     isActive = models.BooleanField()
     
-    
+    class Meta:
+        verbose_name_plural = "Access Properties"
+     
 class SectorPerformanceCategory(models.Model):
     name = models.CharField(max_length=120, null=False, blank=False)
     isActive = models.BooleanField()
     
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = "Performance Categories"
         
+    
+
 class TenderProcedureProperty(models.Model):
     name = models.CharField(max_length=120, null=False, blank=False)
     isActive = models.BooleanField()
     
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = "Tender Procedure Properties"
     
