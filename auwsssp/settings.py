a853ec34946484a524928c08e93125e69d17cabf
Eyassu G <eyassug@gmail.com>
2014-07-25 22:09:57 +0300

    commented out unnecessarily installed django apps

--- auwsssp/settings.py
+++ auwsssp/settings.py
@@ -30,9 +30,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'twitter_bootstrap',
+    #'twitter_bootstrap',
+    #'django_admin_bootstrapped.bootstrap3',
     'django_admin_bootstrapped',    
-    'django_admin_bootstrapped.bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
