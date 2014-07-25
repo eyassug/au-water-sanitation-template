53ddcd321612d60cd5f7b81fe5b749406a9a0f7a
Samuel habtewold <andeabesha@gmail.com>
2014-07-25 11:05:28 +0300

    Modify Settings to Serve static file

--- auwsssp/settings.py
+++ auwsssp/settings.py
@@ -87,3 +87,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+# Emplate Location
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "static", "templates"),
+)
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "auwsssp", "static", "static-only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "auwsssp", "static", "media")
+    STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "auwsssp", "static", "static"),
+)
