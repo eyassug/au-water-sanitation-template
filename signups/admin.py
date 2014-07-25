7c45e7dfb57d9c1408751599d9a4a14af57ebbc0
Samuel habtewold <andeabesha@gmail.com>
2014-07-25 12:32:11 +0300

    Added signups app which is the default for the website

---
+++ signups/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import SignUp 
+
+class SignUpAdmin(admin.ModelAdmin):
+    class Meta:
+        model = SignUp
+        
+        #code
+    
+    #code
+
+admin.site.register(SignUp, SignUpAdmin)
