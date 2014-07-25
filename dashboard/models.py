5cad3501e2c34617eeac7b147911c455b6a148ee
Eyassu G <eyassug@gmail.com>
2014-07-25 20:34:33 +0300

    - Removed commented lines - Corrected verbose names

--- dashboard/models.py
+++ dashboard/models.py
@@ -13,26 +13,12 @@ class PriorityArea(models.Model):
     latitude = models.DecimalField(max_digits=19, decimal_places=10, null=False)
     longitude = models.DecimalField(max_digits=19, decimal_places=10, null=False)
     is_active = models.BooleanField()
+    
+    def __str__(self):
+        return self.name + ' - ' + self.country.name
 
-#class System(models.Model):
-    #name = models.CharField(max_length=120, null=False, blank=False)
-    #is_active = models.BooleanField()
- 
-    #def __str__(self):
-       # return self.name
-    
-#class AccessCharacteristic(models.Model):
-   # system = models.ForeignKey('System')
-    #name = models.CharField(max_length=120, null=False, blank=False)
-    #is_active = models.BooleanField()
-    
-#class AccessProperty(models.Model):
-   # accessCharacteristic = models.ForeignKey('AccessCharacteristic')
-   #name = models.CharField(max_length=120, null=False, blank=False)
-    #is_active = models.BooleanField()
-    
-   # class Meta:
-      #  verbose_name_plural = "Access Properties"
+    class Meta:
+        verbose_name_plural = "Priority Areas"
      
 class SectorCategory(models.Model):
     name = models.CharField(max_length=120, null=False, blank=False)
@@ -48,33 +34,36 @@ class SectorCategory(models.Model):
 
 class TenderProcedureProperty(models.Model):
     sector_category = models.ForeignKey('SectorCategory')
-    tender_proc_property = models.CharField(max_length=120, null=False, blank=False)
+    name = models.CharField(max_length=120, null=False, blank=False)
     is_active = models.BooleanField()
     
     def __str__(self):
-        return self.name
+        return self.name + ' - ' + self.sector_category.name
     
     class Meta:
         verbose_name_plural = "Tender & Procedure Properties"
     
 class Technology(models.Model):
-    sector_category = models.ForeignKey('SectorCategory')
     facility_character = models.ForeignKey('FacilityCharacter')
-    new_technology = models.CharField(max_length=120, null=False, blank=False)
-    
+    name = models.CharField(max_length=120, null=False, blank=False)
+    is_active = models.BooleanField()
+   
     def __str__(self):
-        return self.new_technology;
+        return self.name + ' - ' + self.sector_category.name
+    
     class Meta:
-        verbose_name_plural = "Technology"
+        verbose_name_plural = "Technologies"
         
 class FacilityCharacter(models.Model):
     sector_category = models.ForeignKey('SectorCategory')
-    facility_character = models.CharField(max_length=120, null=False, blank=False)
-    
+    name = models.CharField(max_length=120, null=False, blank=False)
+    is_active = models.BooleanField()
+   
     def __str__(self):
-        return self.facility_character
+        return self.name + ' - ' + self.sector_category.name
+        
     class Meta:
-        verbose_name_plural = "Facility Character"
+        verbose_name_plural = "Facility Characters"
         
 class CountryDemographic(models.Model):
     country = models.ForeignKey('Country')
