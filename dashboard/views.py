7a2ca9b9fa6e1f804124f80fd91dbfdd70583e15
Eyassu G <eyassug@gmail.com>
2014-07-26 20:16:29 +0300

    Added login and logout features

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,8 +4,8 @@ from dashboard.forms import CountryStatusForm, FacilityAccessForm, SectorPerform
 from dashboard.models import CountryDemographic, FacilityAccess, SectorPerformance, PlanningPerformance, TenderProcedurePerformance, CommunityApproach, PartnerContribution, PartnerEventContribution, SWOT
 from django.views.generic import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-
+from django.contrib.auth import authenticate, login, logout
+from dashboard.forms import LoginForm
 # Sector Performance Category
 class SectorPerformanceCreate(CreateView):
     model = SectorPerformance
@@ -42,3 +42,39 @@ class PartnerEventContributionCreate(CreateView):
 class SWOTAndConclusionCreate(CreateView):
     model = SWOT
     template_name = 'swot_form.html'
+    
+class Login(View):
+    def get(self, request):
+        if(request.user.is_authenticated()):
+            return HttpResponseRedirect('/')
+        
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+    
+    def post(self,request):
+        login_form = LoginForm(request.POST)
+        
+        if(login_form.is_valid()):            
+            username = login_form.cleaned_data['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:                    
+                    auth_err = 'The account you specified has been disabled. Please contact your administrator!'
+            else:
+                auth_err = 'Invalid credentials. Please try again!!!'
+        else:
+            return render(request, 'login.html', {'form': login_form})
+        
+        login_form = LoginForm(initial={'username': username})
+        return render(request, 'login.html', {'form': login_form, 'auth_err': auth_err})
+    
+class Logout(View):
+    def get(self,request):
+        if(request.user.is_authenticated()):
+            logout(request)
+        return HttpResponseRedirect('/')
+    
