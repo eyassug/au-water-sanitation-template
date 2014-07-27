3eea647d041298c5edfafc3233d914510719f02e
Eyassu G <eyassug@gmail.com>
2014-07-27 13:22:56 +0300

    added login required on view

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,8 +6,9 @@ from django.views.generic import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import authenticate, login, logout
 from dashboard.forms import LoginForm
+from dashboard.mixins import LoginRequiredMixin
 # Sector Performance Category
-class SectorPerformanceCreate(CreateView):
+class SectorPerformanceCreate(LoginRequiredMixin,CreateView):
     model = SectorPerformance
     template_name = 'sector_performance_form.html'
     
@@ -15,7 +16,7 @@ class FacilityAccessCreate(CreateView):
     model = FacilityAccess
     template_name = 'facility_access_form.html'
     
-class CountryStatusCreate(CreateView):
+class CountryStatusCreate(LoginRequiredMixin,CreateView):
     model = CountryDemographic
     template_name = 'country_status_form.html'
     
@@ -45,15 +46,16 @@ class SWOTAndConclusionCreate(CreateView):
     
 class Login(View):
     def get(self, request):
-        if(request.user.is_authenticated()):
+        auth_err = ''
+        if(request.user.is_authenticated()):            
             return HttpResponseRedirect('/')
         
         form = LoginForm()
-        return render(request, 'login.html', {'form': form})
+        return render(request, 'login.html', {'form': form, 'auth_err': auth_err})
     
     def post(self,request):
         login_form = LoginForm(request.POST)
-        
+        auth_err = ''
         if(login_form.is_valid()):            
             username = login_form.cleaned_data['username']
             password = request.POST['password']
@@ -61,7 +63,10 @@ class Login(View):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponseRedirect('/')
+                    destination_url = '/'
+                    if(request.GET['next']):
+                        dest = request.GET['next']
+                    return HttpResponseRedirect(destination_url)
                 else:                    
                     auth_err = 'The account you specified has been disabled. Please contact your administrator!'
             else:
