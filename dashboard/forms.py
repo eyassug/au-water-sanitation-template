05bc34a3b47572a6eb086bba1917b434b4d7aad5
Eyassu G <eyassug@gmail.com>
2014-07-29 21:07:48 +0300

    Added ajax filtering for Facility Access Form (Form 7)

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from dashboard.models import CountryDemographic, FacilityAccess, SectorPerformance
+from dashboard.models import Country, PriorityArea, SectorCategory
 
 class CountryStatusForm(forms.ModelForm):
     class Meta:
@@ -8,6 +9,7 @@ class CountryStatusForm(forms.ModelForm):
 class FacilityAccessForm(forms.ModelForm):
     class Meta:
         model = FacilityAccess
+        exclude = ['priority_area']
         
 class SectorPerformanceForm(forms.ModelForm):
     class Meta:
@@ -17,4 +19,11 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.PasswordInput()
     
+# Cascade Filters
+class DynamicChoiceField(forms.ChoiceField): 
+    def clean(self, value): 
+        return value
     
+class DFacilityAccessForm(FacilityAccessForm):    
+    country = forms.ModelChoiceField(queryset=Country.objects.all(), widget=forms.Select(attrs={'onchange':'FilterPriorityAreas();'})) 
+    priority_area = DynamicChoiceField(widget=forms.Select(attrs={'disabled':'true'}), choices=(('-1','Select Priority Area'),))
