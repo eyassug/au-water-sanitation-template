9cf4adc3253ce3e9ddf5f33af3a704d1984b84e1
Samuel habtewold <andeabesha@gmail.com>
2014-08-06 12:19:37 +0300

    Added required, unique and default value property on models

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,10 +9,10 @@ class Country(models.Model):
 
 class PriorityArea(models.Model):
     country = models.ForeignKey('Country')
-    name = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
     latitude = models.DecimalField(max_digits=19, decimal_places=10, null=False)
     longitude = models.DecimalField(max_digits=19, decimal_places=10, null=False)
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(default=True)
     
     def __str__(self):
         return self.name + ' - ' + self.country.name
@@ -21,8 +21,8 @@ class PriorityArea(models.Model):
         verbose_name_plural = "Priority Areas"
      
 class SectorCategory(models.Model):
-    name = models.CharField(max_length=120, null=False, blank=False)
-    is_active = models.BooleanField()
+    name = models.CharField(max_length=120, null=False, blank=False, unique=True)
+    is_active = models.BooleanField(default=True)
     
     def __str__(self):
         return self.name
@@ -34,8 +34,8 @@ class SectorCategory(models.Model):
 
 class TenderProcedureProperty(models.Model):
     sector_category = models.ForeignKey('SectorCategory')
-    name = models.CharField(max_length=120, null=False, blank=False)
-    is_active = models.BooleanField()
+    name = models.CharField(max_length=120, null=False, blank=False, unique=True)
+    is_active = models.BooleanField(default=True)
     
     def __str__(self):
         return self.name + ' - ' + self.sector_category.name
@@ -45,8 +45,8 @@ class TenderProcedureProperty(models.Model):
     
 class Technology(models.Model):
     facility_character = models.ForeignKey('FacilityCharacter')
-    name = models.CharField(max_length=120, null=False, blank=False)
-    is_active = models.BooleanField()
+    name = models.CharField(max_length=120, null=False, blank=False, unique=True)
+    is_active = models.BooleanField(default=True)
    
     def __str__(self):
         return self.name + ' - ' + self.facility_character.name + ' - ' + self.facility_character.sector_category.name
@@ -56,8 +56,8 @@ class Technology(models.Model):
         
 class FacilityCharacter(models.Model):
     sector_category = models.ForeignKey('SectorCategory')
-    name = models.CharField(max_length=120, null=False, blank=False)
-    is_active = models.BooleanField()
+    name = models.CharField(max_length=120, null=False, blank=False, unique=True)
+    is_active = models.BooleanField(default=True)
    
     def __str__(self):
         return self.name + ' - ' + self.sector_category.name
@@ -66,8 +66,8 @@ class FacilityCharacter(models.Model):
         verbose_name_plural = "Facility Characters"
     
 class CommunityApproachType(models.Model):
-    name = models.CharField(max_length=120, null=False, blank=False)
-    is_active = models.BooleanField()
+    name = models.CharField(max_length=120, null=False, blank=False, unique=True)
+    is_active = models.BooleanField(default=True)
     
     def __str__(self):
         return self.name
@@ -76,50 +76,50 @@ class CommunityApproachType(models.Model):
         verbose_name_plural = "Community Approach Types"
     
 class Partner(models.Model):
-    name = models.CharField(max_length=120, null=False, blank=False)
+    name = models.CharField(max_length=120, null=False, blank=False, unique=True)
     mission = models.TextField()
     key_activities = models.TextField()
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(default=True)
     
     def __str__(self):
         return self.name
 
 class Event(models.Model):
-    name = models.CharField(max_length=120, null=False, blank=False)
+    name = models.CharField(max_length=120, null=False, blank=False, unique=True)
     date = models.DateField()
-    venue = models.CharField(max_length=120, null=False, blank=False)
+    venue = models.CharField(max_length=120, null=False)
     
     def __str__(self):
         return self.name + ' - ' + self.venue
     
 
 class CountryDemographic(models.Model):
-    country = models.ForeignKey('Country')
-    year = models.PositiveSmallIntegerField(null=False,blank=False)
+    country = models.ForeignKey('Country', unique=True)
+    year = models.PositiveSmallIntegerField(null=False,blank=False, unique=True)
     population = models.IntegerField(null=False,blank=False)
     
 class PriorityAreaStatus(models.Model):
     prioriry_area = models.ForeignKey('PriorityArea')
     year = models.PositiveIntegerField()
-    population = models.IntegerField()
+    population = models.PositiveIntegerField()
     number_of_households = models.IntegerField()
     
 class FacilityAccess(models.Model):
-    priority_area = models.ForeignKey('PriorityArea')
-    technology = models.ForeignKey('Technology')
+    priority_area = models.ForeignKey('PriorityArea', blank=False)
+    technology = models.ForeignKey('Technology', blank=False)
     year = models.PositiveSmallIntegerField(null=False,blank=False)
-    planned = models.DecimalField(max_digits=19, decimal_places=10, null=False)
-    actual = models.DecimalField(max_digits=19, decimal_places=10, null=False)
-    unit_cost = models.DecimalField(max_digits=19, decimal_places=10, null=False)
-    house_hold_contribution = models.DecimalField(max_digits=5, decimal_places=2, null=False)
-    government_contribution = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+    planned = models.DecimalField(max_digits=19, decimal_places=2, null=False)
+    actual = models.DecimalField(max_digits=19, decimal_places=2, null=False)
+    unit_cost = models.DecimalField(max_digits=19, decimal_places=2, null=False)
+    house_hold_contribution = models.DecimalField(max_digits=3, decimal_places=2, null=False)
+    government_contribution = models.DecimalField(max_digits=3, decimal_places=2, null=False)
     
 class SectorPerformance(models.Model):
-    country = models.ForeignKey('Country')
-    sector_category = models.ForeignKey('SectorCategory')
+    country = models.ForeignKey('Country', blank=False)
+    sector_category = models.ForeignKey('SectorCategory', blank=False)
     year = models.PositiveSmallIntegerField(null=False,blank=False)
-    coverage_target = models.DecimalField(max_digits=5, decimal_places=2, null=False)
-    coverage_achieved = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+    coverage_target = models.DecimalField(max_digits=3, decimal_places=2, null=False)
+    coverage_achieved = models.DecimalField(max_digits=3, decimal_places=2, null=False)
     fund_needed = models.DecimalField(max_digits=15, decimal_places=2, null=False)
     fund_mobilised = models.DecimalField(max_digits=15, decimal_places=2, null=False)
     fund_availed = models.DecimalField(max_digits=15, decimal_places=2, null=False)
@@ -130,8 +130,8 @@ class SectorPerformance(models.Model):
     success_challenges = models.TextField()
     
 class PlanningPerformance(models.Model):
-    country = models.ForeignKey('Country')
-    sector_category = models.ForeignKey('SectorCategory')
+    country = models.ForeignKey('Country', blank=False)
+    sector_category = models.ForeignKey('SectorCategory', blank=False)
     year = models.PositiveSmallIntegerField(null=False,blank=False)
     plan_preparation_delay = models.PositiveSmallIntegerField(null=False,blank=False)    
     plan_adoption_delay = models.PositiveSmallIntegerField(null=False,blank=False)
@@ -141,10 +141,10 @@ class PlanningPerformance(models.Model):
     success_challenges = models.TextField()
 
 class TenderProcedurePerformance(models.Model):
-    country = models.ForeignKey('Country')
-    sector_category = models.ForeignKey('SectorCategory')
+    country = models.ForeignKey('Country',blank=False)
+    sector_category = models.ForeignKey('SectorCategory', blank=False)
     year = models.PositiveSmallIntegerField(null=False,blank=False)
-    tender_procedure_property = models.ForeignKey('TenderProcedureProperty')
+    tender_procedure_property = models.ForeignKey('TenderProcedureProperty', blank=False)
     registered = models.PositiveSmallIntegerField(null=False,blank=False)
     executed = models.PositiveSmallIntegerField(null=False,blank=False)
     general_comment = models.TextField()
@@ -153,36 +153,36 @@ class TenderProcedurePerformance(models.Model):
     success_challenges = models.TextField()
 
 class CommunityApproach(models.Model):
-    country = models.ForeignKey('Country')
-    sector_category = models.ForeignKey('SectorCategory')
+    country = models.ForeignKey('Country', blank=False)
+    sector_category = models.ForeignKey('SectorCategory', blank=False)
     year = models.PositiveSmallIntegerField(null=False,blank=False)
-    approach_type = models.ForeignKey('CommunityApproachType')
+    approach_type = models.ForeignKey('CommunityApproachType',blank=False)
     approach_name = models.CharField(max_length=120, null=False, blank=False)    
     description = models.TextField()
     cost_per_capita = models.DecimalField(max_digits=15, decimal_places=4, null=False)
     lessons_learnt = models.TextField()
     
 class PartnerContribution(models.Model):
-    country = models.ForeignKey('Country')
-    sector_category = models.ForeignKey('SectorCategory')
-    partner = models.ForeignKey('Partner')
+    country = models.ForeignKey('Country',blank=False)
+    sector_category = models.ForeignKey('SectorCategory', blank=False)
+    partner = models.ForeignKey('Partner', blank=False)
     annual_contribution = models.DecimalField(max_digits=15, decimal_places=4, null=False)
     in_kind_contribution = models.DecimalField(max_digits=5, decimal_places=2, null=False)
     financial_contribution = models.DecimalField(max_digits=5, decimal_places=2, null=False)
     
 class PartnerEventContribution(models.Model):
-    country = models.ForeignKey('Country')
-    sector_category = models.ForeignKey('SectorCategory')
+    country = models.ForeignKey('Country', blank=False)
+    sector_category = models.ForeignKey('SectorCategory', blank=False)
     year = models.PositiveSmallIntegerField(null=False,blank=False)
-    partner = models.ForeignKey('Partner')
-    event = models.ForeignKey('Event')
-    government_staff_contribution = models.DecimalField(max_digits=15, decimal_places=4, null=False)
-    own_staff_contribution = models.DecimalField(max_digits=15, decimal_places=4, null=False)
-    total_event_contribution = models.DecimalField(max_digits=15, decimal_places=4, null=False)
+    partner = models.ForeignKey('Partner', blank=False)
+    event = models.ForeignKey('Event', blank=False)
+    government_staff_contribution = models.DecimalField(max_digits=3, decimal_places=2, null=False)
+    own_staff_contribution = models.DecimalField(max_digits=3, decimal_places=2, null=False)
+    total_event_contribution = models.DecimalField(max_digits=3, decimal_places=2, null=False)
     
 class SWOT(models.Model):
-    country = models.ForeignKey('Country')
-    sector_category = models.ForeignKey('SectorCategory')
+    country = models.ForeignKey('Country', blank=False)
+    sector_category = models.ForeignKey('SectorCategory', blank=False)
     overall_challenges = models.TextField()
     strengths = models.TextField()
     weaknesses = models.TextField()
