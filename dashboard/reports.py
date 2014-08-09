d0b7fbd882a53f913d736d31a11a8ab5e57ec2b2
Eyassu G <eyassug@gmail.com>
2014-08-09 21:37:34 +0300

    Implemented PriorityArea List Report

--- dashboard/reports.py
+++ dashboard/reports.py
@@ -1,14 +1,19 @@
 from dashboard import models
+from django.db.models import Sum
 
-class PriorityAreaPopultionReport():
+class PriorityAreaPopulationReport():
     
-    def generate(country=None):    
-        priority_areas = models.PriorityArea.objects.filter(country=self.country)
+    def generate(self,country=None):    
+        priority_areas = models.PriorityArea.objects.filter(country=country)
         population = models.PriorityAreaStatus.objects.filter(priority_area__country=country)
+        total_population = models.PriorityAreaStatus.objects.aggregate(Sum('population'))
+        total_households = models.PriorityAreaStatus.objects.aggregate(Sum('number_of_households'))
         
         return {
             'priority_areas':priority_areas,
-            'population':population
+            'population':population,
+            'total_population':total_population['population__sum'],
+            'total_households':total_households['number_of_households__sum']
         }
             
     
