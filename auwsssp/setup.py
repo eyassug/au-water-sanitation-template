ba35991a9989a548ef3f0d794ee4904d6322a6ff
Eyassu G <eyassug@gmail.com>
2014-08-18 00:25:22 +0300

    Added setup.py file to seed data on first run

---
+++ auwsssp/setup.py
@@ -0,0 +1,26 @@
+from dashboard import models
+
+def seed_data():
+    seed_countries()
+    seed_sector_categories()
+    seed_facility_characters()
+    seed_technologies()
+    seed_periods()
+
+def seed_countries():
+    pass
+
+def seed_sector_categories():
+    pass
+
+def seed_facility_characters():
+    pass
+
+def seed_technologies():
+    pass
+
+def seed_periods():
+    pass
+
+
+
