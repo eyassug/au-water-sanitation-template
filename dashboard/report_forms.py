8fe4444c4ce73ca67814ce7ecde432d8764698c4
Eyassu G <eyassug@gmail.com>
2014-08-20 01:24:03 +0300

    Added cascaded filters for Tech Gap report by PA

--- dashboard/report_forms.py
+++ dashboard/report_forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from dashboard import models
+from dashboard.forms import DynamicChoiceField
 
 YEAR_RANGE = [(i,i) for i in range(1990,2031)]
 
 class TechnologyGapByPriorityArea(forms.Form):
-    technology = forms.ModelChoiceField(required=True,queryset=models.Technology.objects.all(), widget=forms.Select())
+    sector_category = forms.ModelChoiceField(queryset=models.SectorCategory.objects.all(), widget=forms.Select(attrs={'onchange':'FilterFacilityCharacters();'}))
+    facility_character = DynamicChoiceField(widget=forms.Select(attrs={'onchange':'FilterTechnologies();', 'disabled':'true'}), choices=(('-1','Select Facility Character'),))
+    technology = DynamicChoiceField(widget=forms.Select(attrs={'disabled':'true'}), choices=(('-1','Select Technology'),))
     start_year = forms.ChoiceField(choices=YEAR_RANGE)
     end_year = forms.ChoiceField(choices=YEAR_RANGE)
     
