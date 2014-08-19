ed48739eb0d5198e309be3940d37ddbc16f59ba1
Samuel habtewold <andeabesha@gmail.com>
2014-08-19 23:45:18 +0300

    Added filter feature for Technology Gaps Clustered By Priority Area report

--- dashboard/report_forms.py
+++ dashboard/report_forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from dashboard import models
+from dashboard.models import Country, PriorityArea, SectorCategory, TenderProcedurePerformance, TenderProcedureProperty
 
 YEAR_RANGE = [(i,i) for i in range(1990,2031)]
 
+class DynamicChoiceField(forms.ChoiceField): 
+    def clean(self, value): 
+        return value
 class TechnologyGapByPriorityArea(forms.Form):
+    sector_category = forms.ModelChoiceField(required=False,queryset=SectorCategory.objects.all(), widget=forms.Select(attrs={'onchange':'FilterFacilityCharacters();'}))
+    facility_character = DynamicChoiceField(widget=forms.Select(attrs={'onchange':'FilterTechnologiesNew();'}),)
     technology = forms.ModelChoiceField(required=True,queryset=models.Technology.objects.all(), widget=forms.Select())
     start_year = forms.ChoiceField(choices=YEAR_RANGE)
     end_year = forms.ChoiceField(choices=YEAR_RANGE)
